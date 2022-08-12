05af0a0220874a9769efd916a28bf7ab684cf579
Md Ashraful Alam Hazari <farhan.hazari@gmail.com>
2022-08-12 23:14:34 +0200

    Implemented Kafka Producer

--- codes/kafka_weather_producer.py
+++ codes/kafka_weather_producer.py
@@ -1,10 +1,7 @@
 from pickle import TRUE
 import sys
 import time
-import json
-import random
-from datetime import datetime
-from weather_data_generator import get_weather_records, fetch_weather_data
+from weather_data_generator import fetch_weather_data
 #from kafka import KafkaProducer
 from confluent_kafka import Producer
 
@@ -31,13 +28,12 @@ if __name__ == '__main__':
 
     while TRUE:
         # Generate a kafka weather_data message
-        weather_data_message = get_weather_records()
+        weather_data_message = fetch_weather_data()
 
         print("Response: ", weather_data_message)
         # Send it to our 'fcz61y67-weather_data' topic
         # send message to topic in json format
         try:
-            # p.produce(topic[0], json.dumps(accident_message).encode('utf-8'))
             p.produce(topic[0], weather_data_message.encode('utf-8'), callback=delivery_callback)
         except BufferError as e:
             sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
@@ -49,7 +45,6 @@ if __name__ == '__main__':
         p.poll(0)
 
         # Sleep for a random number of seconds
-        # time_to_sleep = random.randint(0, 10)
         print('Weather message produced. . .')
-        time_to_sleep = 5
+        time_to_sleep = 300
         time.sleep(time_to_sleep)
